781113a8686c6764c623500471bd838990457701
Anupam Godbole <anupam@soundhound.com>
2022-10-13 16:01:47 -0700

    Added support for showing thumbnail on iPhone and compact iPad

--- PDF Manipulator/OpenedPDF/PDFPagesViewController.swift
+++ PDF Manipulator/OpenedPDF/PDFPagesViewController.swift
@@ -20,14 +20,21 @@ final class PDFPagesViewController: UIHostingController<PDFPagesViewController.O
         super.init(rootView: OuterPDFMainView(pdfDoc: pdfDoc, scene: scene))
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //self.navigationController?.navigationBar.isTranslucent = false
-    }
-
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func showSidebar() {
+        let thumbnailVC = PDFThumbnailsViewController(pdfDoc: pdfDoc, scene: scene)
+        thumbnailVC.modalPresentationStyle = .custom
+        thumbnailVC.transitioningDelegate = self
+        self.present(thumbnailVC, animated: true)
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if previousTraitCollection?.horizontalSizeClass == .compact && self.traitCollection.horizontalSizeClass == .regular {
+            self.presentedViewController?.dismiss(animated: false)
+        }
     }
 
     struct OuterPDFMainView: View {
@@ -117,7 +124,22 @@ final class PDFPagesViewController: UIHostingController<PDFPagesViewController.O
                     } label: {
                         Image(systemName: hidePrimaryColumn ? "arrow.up.left.and.arrow.down.right" : "arrow.down.right.and.arrow.up.left")
                     }
-                    .opacity(horizontalSizeClass == .compact ? 0 : 1)
+                    .opacity(horizontalSizeClass == .compact || UIDevice.current.userInterfaceIdiom == .phone ? 0 : 1)
+                }
+                
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        let pdfPagesVC: PDFPagesViewController?
+                        if UIDevice.current.userInterfaceIdiom == .pad {
+                            pdfPagesVC = ((scene?.keyWindow?.rootViewController as? SplitViewController)?.viewControllers[0] as? UINavigationController)?.topViewController as? PDFPagesViewController
+                        } else {
+                            pdfPagesVC = (scene?.keyWindow?.rootViewController as? UINavigationController)?.topViewController as? PDFPagesViewController
+                        }
+                        pdfPagesVC?.showSidebar()
+                    } label: {
+                        Image(systemName: "sidebar.squares.right")
+                    }
+                    .opacity(horizontalSizeClass == .compact || UIDevice.current.userInterfaceIdiom == .phone ? 1 : 0)
                 }
             }
         }
@@ -151,5 +173,65 @@ final class PDFPagesViewController: UIHostingController<PDFPagesViewController.O
                 }
             }
         }
+    }
+}
+
+extension PDFPagesViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self
+    }
+}
+
+extension PDFPagesViewController: UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        0.4
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let toVC = transitionContext.viewController(forKey: .to)!
+        if toVC.isBeingPresented == true {
+            let bounds = transitionContext.containerView.bounds
+            let backgroundCancelButton = UIButton(frame: bounds)
+            backgroundCancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+            backgroundCancelButton.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+            backgroundCancelButton.backgroundColor = .black
+            backgroundCancelButton.alpha = 0
+            backgroundCancelButton.tag = 1
+            transitionContext.containerView.addSubview(backgroundCancelButton)
+            
+            var frame = transitionContext.finalFrame(for: toVC)
+            frame.origin.x = frame.width
+            frame.size.width -= 100
+            let toView = transitionContext.view(forKey: .to)!
+            toView.frame = frame
+            toView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+            transitionContext.containerView.addSubview(toView)
+            
+            UIView.animate(withDuration: self.transitionDuration(using: transitionContext)) {
+                backgroundCancelButton.alpha = 0.7
+                frame.origin.x -= frame.width
+                toView.frame = frame
+            } completion: { completed in
+                transitionContext.completeTransition(completed)
+            }
+        } else {
+            UIView.animate(withDuration: self.transitionDuration(using: transitionContext)) {
+                let fromVC = transitionContext.viewController(forKey: .from)!
+                var frame = transitionContext.initialFrame(for: fromVC)
+                transitionContext.containerView.viewWithTag(1)?.alpha = 0
+                frame.origin.x += frame.width
+                transitionContext.view(forKey: .from)?.frame = frame
+            } completion: { completed in
+                transitionContext.completeTransition(completed)
+            }
+        }
+    }
+    
+    @objc private func cancelButtonTapped() {
+        self.presentedViewController?.dismiss(animated: true)
     }
 }
